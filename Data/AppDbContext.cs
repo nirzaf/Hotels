b852dc69789af7c96c6fcbfc4393775375c71abf
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-30 18:57:00 +0530

    DB Seeding

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -16,21 +16,45 @@ namespace Hotels.Model
         {
             base.OnModelCreating(builder);
             builder.Entity<Country>().HasData(new Country
-            {
-                Id = 1,
-                Name = "Sri Lanka",
-                ShortName = "SL"
-            },
-            new Country{
-                Id = 2,
-                Name = "India",
-                ShortName = "IN"
-            }, new Country
-            {
-                Id = 3,
-                Name = "Canada",
-                ShortName = "CN"
-            });
+                {
+                    Id = 1,
+                    Name = "Sri Lanka",
+                    ShortName = "SL"
+                },
+                new Country{
+                    Id = 2,
+                    Name = "India",
+                    ShortName = "IN"
+                }, new Country
+                {
+                    Id = 3,
+                    Name = "Canada",
+                    ShortName = "CN"
+                });
+
+            builder.Entity<Hotel>().HasData(new Hotel
+                {
+                    Id = 1,
+                    Name = "Queens Hotel",
+                    Address = "Kandy",
+                    Rating = 4,
+                    CountryId = 1 
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Taj Hotel",
+                    Address = "Chennai",
+                    Rating = 3.5,
+                    CountryId = 2
+                }, new Hotel
+                {
+                    Id = 3,
+                    Name = "British Columbia",
+                    Address = "BC Canada",
+                    Rating = 5,
+                    CountryId = 3
+                });
         }
     }
 }
--- Migrations/20210530132526_seeding-data.Designer.cs
+++ Migrations/20210530132526_seeding-data.Designer.cs
@@ -3,14 +3,16 @@ using Hotels.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Hotels.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210530132526_seeding-data")]
+    partial class seedingdata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
