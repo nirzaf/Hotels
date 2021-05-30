abe4a9822626cb196f9f99629c26a3670f118117
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-30 19:56:37 +0530

    Unit Of Work Implemented

---
+++ IRepository/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Hotels.Data;
+
+namespace Hotels.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Country> Countries { get;}
+        IGenericRepository<Hotel> Hotels { get; }
+        Task Save();
+    }
+}
